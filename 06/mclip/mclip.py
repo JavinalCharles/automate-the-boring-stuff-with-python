364b78972a7248da729d425dc7af5fbc6c0c2da5
Charlemagne Javinal <javinal.charlemagne@gmail.com>
2023-09-09 23:53:14 +0800

    Chapter 6 Project: Multi-clipboard automatic message.

---
+++ 06/mclip/mclip.py
@@ -0,0 +1,27 @@
+#!/usr/bin python3
+# mclip.py - A multi-clipboard program
+
+TEXT = {
+	'agree': 
+		"""Yes, I agree. That sounds fine to me.""",
+    'busy': 
+		"""Sorry, can we do this later this week or next week?""",
+    'upsell': 
+		"""Would you consider making this a monthly donation?"""
+	}
+
+if __name__ == "__main__":
+	import sys
+	import pyperclip
+
+	if len(sys.argv) < 2:
+		print('Usage: python mclip.py [keyphrase] - copy phrase text')
+		sys.exit()
+
+	keyphrase = sys.argv[1]	# First command line arge is the keyphrase
+
+	if keyphrase in TEXT:
+		pyperclip.copy(TEXT[keyphrase])
+		print('Text for ' + keyphrase + ' copied to clipboard.')
+	else:
+		print('There is no text for ' + keyphrase)
